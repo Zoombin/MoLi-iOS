a849d276650e3ac984583df2585fe8a98ffeffb3
Bin Zhang <30135878@qq.com>
2015-04-12 12:07:18 +0800

    全部订单、订单详情添加了商品属性 （接口已经更新了 多了goods.spec）

--- MoLi-iOS/MoLi/MLGoodsOrderTableViewCell.m
+++ MoLi-iOS/MoLi/MLGoodsOrderTableViewCell.m
@@ -15,6 +15,7 @@
 @property (readwrite) VerticallyAlignedLabel *nameLabel;
 @property (readwrite) UILabel *priceLabel;
 @property (readwrite) UILabel *numberLabel;
+@property (readwrite) UILabel *propertiesLabel;
 
 @end
 
@@ -29,39 +30,45 @@
     if (self) {
         UIView *line = [[UIView alloc] initWithFrame:CGRectMake(10, 0, WINSIZE.width - 20, 1)];
         line.backgroundColor = [UIColor colorWithRed:0.88 green:0.88 blue:0.88 alpha:1];
-        [self addSubview:line];
+        [self.contentView addSubview:line];
         
         _iconView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 10, 60, 60)];
         _iconView.layer.borderColor = [UIColor colorWithRed:0.84 green:0.84 blue:0.84 alpha:1].CGColor;
         _iconView.layer.borderWidth = 0.5;
         _iconView.contentMode = UIViewContentModeScaleAspectFit;
-        [self addSubview:_iconView];
+        [self.contentView addSubview:_iconView];
         
         float x = _iconView.x + _iconView.width + 10;
         float width = WINSIZE.width - x - 20;
         
         _nameLabel = [[VerticallyAlignedLabel alloc] init];
         [_nameLabel setVerticalAlignment:VerticalAlignmentTop];
-        _nameLabel.frame = CGRectMake(x, _iconView.frame.origin.y, width, 40);
-        _nameLabel.font = [UIFont systemFontOfSize:16];
+        _nameLabel.frame = CGRectMake(x, _iconView.frame.origin.y, width, 20);
+        _nameLabel.font = [UIFont systemFontOfSize:15];
         _nameLabel.lineBreakMode = NSLineBreakByTruncatingTail;
         _nameLabel.textColor = [UIColor colorWithRed:0.43 green:0.43 blue:0.43 alpha:1];
-        _nameLabel.numberOfLines = 2;
-        [self addSubview:_nameLabel];
+        _nameLabel.numberOfLines = 1;
+		_nameLabel.adjustsFontSizeToFitWidth = YES;
+        [self.contentView addSubview:_nameLabel];
         
         _priceLabel = [[UILabel alloc] init];
-        _priceLabel.frame = CGRectMake(x, _iconView.frame.origin.y + _iconView.frame.size.height - 15, width, 15);
-        _priceLabel.font = [UIFont systemFontOfSize:14];
+        _priceLabel.frame = CGRectMake(x, CGRectGetMaxY(_nameLabel.frame) + 5, width, 15);
+        _priceLabel.font = [UIFont systemFontOfSize:13];
         _priceLabel.textColor = [UIColor colorWithRed:0.53 green:0.53 blue:0.53 alpha:1];
-        [self addSubview:_priceLabel];
+        [self.contentView addSubview:_priceLabel];
         
         _numberLabel = [[UILabel alloc] init];
         _numberLabel.frame = CGRectMake(self.frame.size.width - 120, _priceLabel.frame.origin.y, 100, 15);
-        _numberLabel.font = [UIFont systemFontOfSize:14];
+		_numberLabel.font = _priceLabel.font;
         _numberLabel.textAlignment = NSTextAlignmentRight;
         _numberLabel.textColor = [UIColor colorWithRed:0.53 green:0.53 blue:0.53 alpha:1];
-        [self addSubview:_numberLabel];
-        
+        [self.contentView addSubview:_numberLabel];
+		
+		_propertiesLabel = [[UILabel alloc] init];
+		_propertiesLabel.frame = CGRectMake(x, CGRectGetMaxY(_priceLabel.frame) + 5, width, 15);
+		_propertiesLabel.font = _numberLabel.font;
+		_propertiesLabel.textColor = _numberLabel.textColor;
+		[self.contentView addSubview:_propertiesLabel];
     }
     return self;
 }
@@ -73,6 +80,7 @@
         _priceLabel.text = [NSString stringWithFormat:@"价格:¥%0.2f", [_goods.price floatValue]];
         _numberLabel.text = [NSString stringWithFormat:@"数量:%@", _goods.quantityBought];
         _nameLabel.text = _goods.name;
+		_propertiesLabel.text = _goods.goodsPropertiesString;
 	}
 }
 
