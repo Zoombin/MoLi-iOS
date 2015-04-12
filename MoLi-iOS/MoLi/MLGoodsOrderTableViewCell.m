e55cc381dcd2a536334e3c18206ea88420c0d3d6
Bin Zhang <30135878@qq.com>
2015-04-12 11:59:30 +0800

    Clean codes.

--- MoLi-iOS/MoLi/MLGoodsOrderTableViewCell.m
+++ MoLi-iOS/MoLi/MLGoodsOrderTableViewCell.m
@@ -9,18 +9,16 @@
 #import "MLGoodsOrderTableViewCell.h"
 #import "Header.h"
 
-@interface MLGoodsOrderTableViewCell() {
-    UIImageView *mImageView;
-    VerticallyAlignedLabel *mTitleLabel;
-    UILabel *mPriceLabel;
-    UILabel *mNumberLabel;
-}
+@interface MLGoodsOrderTableViewCell()
+
+@property (readwrite) UIImageView *iconView;
+@property (readwrite) VerticallyAlignedLabel *nameLabel;
+@property (readwrite) UILabel *priceLabel;
+@property (readwrite) UILabel *numberLabel;
 
 @end
 
 @implementation MLGoodsOrderTableViewCell
-
-
 
 + (CGFloat)height {
 	return 80;
@@ -29,43 +27,40 @@
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        
-        // 添加横线
         UIView *line = [[UIView alloc] initWithFrame:CGRectMake(10, 0, WINSIZE.width - 20, 1)];
         line.backgroundColor = [UIColor colorWithRed:0.88 green:0.88 blue:0.88 alpha:1];
         [self addSubview:line];
         
-        mImageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 10, 60, 60)];
-        //        mImageView.image = [UIImage imageNamed:@"Placeholder"];
-        mImageView.layer.borderColor = [UIColor colorWithRed:0.84 green:0.84 blue:0.84 alpha:1].CGColor;
-        mImageView.layer.borderWidth = 0.5;
-        mImageView.contentMode = UIViewContentModeScaleAspectFit;
-        [self addSubview:mImageView];
+        _iconView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 10, 60, 60)];
+        _iconView.layer.borderColor = [UIColor colorWithRed:0.84 green:0.84 blue:0.84 alpha:1].CGColor;
+        _iconView.layer.borderWidth = 0.5;
+        _iconView.contentMode = UIViewContentModeScaleAspectFit;
+        [self addSubview:_iconView];
         
-        float x = mImageView.x + mImageView.width + 10;
+        float x = _iconView.x + _iconView.width + 10;
         float width = WINSIZE.width - x - 20;
         
-        mTitleLabel = [[VerticallyAlignedLabel alloc] init];
-        [mTitleLabel setVerticalAlignment:VerticalAlignmentTop];
-        mTitleLabel.frame = CGRectMake(x, mImageView.frame.origin.y, width, 40);
-        mTitleLabel.font = [UIFont systemFontOfSize:16];
-        mTitleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
-        mTitleLabel.textColor = [UIColor colorWithRed:0.43 green:0.43 blue:0.43 alpha:1];
-        mTitleLabel.numberOfLines = 2;
-        [self addSubview:mTitleLabel];
+        _nameLabel = [[VerticallyAlignedLabel alloc] init];
+        [_nameLabel setVerticalAlignment:VerticalAlignmentTop];
+        _nameLabel.frame = CGRectMake(x, _iconView.frame.origin.y, width, 40);
+        _nameLabel.font = [UIFont systemFontOfSize:16];
+        _nameLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+        _nameLabel.textColor = [UIColor colorWithRed:0.43 green:0.43 blue:0.43 alpha:1];
+        _nameLabel.numberOfLines = 2;
+        [self addSubview:_nameLabel];
         
-        mPriceLabel = [[UILabel alloc] init];
-        mPriceLabel.frame = CGRectMake(x, mImageView.frame.origin.y + mImageView.frame.size.height - 15, width, 15);
-        mPriceLabel.font = [UIFont systemFontOfSize:14];
-        mPriceLabel.textColor = [UIColor colorWithRed:0.53 green:0.53 blue:0.53 alpha:1];
-        [self addSubview:mPriceLabel];
+        _priceLabel = [[UILabel alloc] init];
+        _priceLabel.frame = CGRectMake(x, _iconView.frame.origin.y + _iconView.frame.size.height - 15, width, 15);
+        _priceLabel.font = [UIFont systemFontOfSize:14];
+        _priceLabel.textColor = [UIColor colorWithRed:0.53 green:0.53 blue:0.53 alpha:1];
+        [self addSubview:_priceLabel];
         
-        mNumberLabel = [[UILabel alloc] init];
-        mNumberLabel.frame = CGRectMake(self.frame.size.width - 120, mPriceLabel.frame.origin.y, 100, 15);
-        mNumberLabel.font = [UIFont systemFontOfSize:14];
-        mNumberLabel.textAlignment = NSTextAlignmentRight;
-        mNumberLabel.textColor = [UIColor colorWithRed:0.53 green:0.53 blue:0.53 alpha:1];
-        [self addSubview:mNumberLabel];
+        _numberLabel = [[UILabel alloc] init];
+        _numberLabel.frame = CGRectMake(self.frame.size.width - 120, _priceLabel.frame.origin.y, 100, 15);
+        _numberLabel.font = [UIFont systemFontOfSize:14];
+        _numberLabel.textAlignment = NSTextAlignmentRight;
+        _numberLabel.textColor = [UIColor colorWithRed:0.53 green:0.53 blue:0.53 alpha:1];
+        [self addSubview:_numberLabel];
         
     }
     return self;
@@ -74,10 +69,10 @@
 - (void)setGoods:(MLGoods *)goods {
 	_goods = goods;
     if (_goods) {
-        [mImageView setImageWithURL:[NSURL URLWithString:goods.imagePath] placeholderImage:[UIImage imageNamed:@"Placeholder"]];
-        mPriceLabel.text = [NSString stringWithFormat:@"价格:¥%0.2f", [_goods.price floatValue]];
-        mNumberLabel.text = [NSString stringWithFormat:@"数量:%@", _goods.quantityBought];
-        mTitleLabel.text = _goods.name;
+        [_iconView setImageWithURL:[NSURL URLWithString:goods.imagePath] placeholderImage:[UIImage imageNamed:@"Placeholder"]];
+        _priceLabel.text = [NSString stringWithFormat:@"价格:¥%0.2f", [_goods.price floatValue]];
+        _numberLabel.text = [NSString stringWithFormat:@"数量:%@", _goods.quantityBought];
+        _nameLabel.text = _goods.name;
 	}
 }
 
