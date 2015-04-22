bb2f78ed112318fb31fc65b99c8a0258b2d33eeb
ycabc1989 <yc@ycdeMac.local>
2015-04-22 17:50:33 +0800

    修改评价界面(功能待完善)

--- MoLi-iOS/MoLi/MLJudgeViewController.h
+++ MoLi-iOS/MoLi/MLJudgeViewController.h
@@ -14,18 +14,18 @@
 @interface MLJudgeViewController : UIViewController<UITextViewDelegate, UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (nonatomic, weak) MLOrder *order;
-@property (nonatomic, weak) IBOutlet UIView *footView;
+//@property (nonatomic, weak) IBOutlet UIView *footView;+
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
-@property (nonatomic, weak) IBOutlet UITextView *commentTextView;
+//@property (nonatomic, weak) IBOutlet UITextView *commentTextView;
 @property (nonatomic, weak) IBOutlet UILabel *placeholderLabel;
-@property (nonatomic, weak) IBOutlet UIButton *photo1;
-@property (nonatomic, weak) IBOutlet UIButton *photo2;
-@property (nonatomic, weak) IBOutlet UIButton *photo3;
-@property (nonatomic, weak) IBOutlet UIButton *star1;
-@property (nonatomic, weak) IBOutlet UIButton *star2;
-@property (nonatomic, weak) IBOutlet UIButton *star3;
-@property (nonatomic, weak) IBOutlet UIButton *star4;
-@property (nonatomic, weak) IBOutlet UIButton *star5;
+//@property (nonatomic, weak) IBOutlet UIButton *photo1;
+//@property (nonatomic, weak) IBOutlet UIButton *photo2;
+//@property (nonatomic, weak) IBOutlet UIButton *photo3;
+//@property (nonatomic, weak) IBOutlet UIButton *star1;
+//@property (nonatomic, weak) IBOutlet UIButton *star2;
+//@property (nonatomic, weak) IBOutlet UIButton *star3;
+//@property (nonatomic, weak) IBOutlet UIButton *star4;
+//@property (nonatomic, weak) IBOutlet UIButton *star5;
 - (IBAction)starButtonClicked:(id)sender;
 - (IBAction)sendComment:(id)sender;
 - (IBAction)selectPhoto:(id)sender;
--- MoLi-iOS/MoLi/MLJudgeViewController.m
+++ MoLi-iOS/MoLi/MLJudgeViewController.m
@@ -8,6 +8,7 @@
 
 #import "MLJudgeViewController.h"
 #import "MLGoodsOrderTableViewCell.h"
+#import "MLJudgeGoodsCell.h"
 
 @interface MLJudgeViewController ()
 
@@ -30,57 +31,57 @@
     self.title = @"评价订单";
     star = @"5";
     [self setLeftBarButtonItemAsBackArrowButton];
-    _commentTextView.backgroundColor = [UIColor backgroundColor];
-    _commentTextView.delegate = self;
-    [_commentTextView.layer setCornerRadius:4.0];
-    [_commentTextView.layer setMasksToBounds:YES];
+//    _commentTextView.backgroundColor = [UIColor backgroundColor];
+//    _commentTextView.delegate = self;
+//    [_commentTextView.layer setCornerRadius:4.0];
+//    [_commentTextView.layer setMasksToBounds:YES];
     imagePaths = [NSMutableArray array];
-    [_tableView setTableFooterView:_footView];
+//    [_tableView setTableFooterView:_footView];
     [self showInfo];
-    [self addDeleteIcons];
+//    [self addDeleteIcons];
 }
 
-// 为图片添加删除图标
-- (void)addDeleteIcons
-{
-    UIImage *deleteImage = [UIImage imageNamed:@"DeleteUploadedImage"];
-    self.imgviewDel1 = [[UIImageView alloc] initWithImage:deleteImage];
-    [self.imgviewDel1 setFrame:CGRectMake(0, 0, 15, 15)];
-    self.imgviewDel1.center = CGPointMake(CGRectGetMaxX(_photo1.bounds), 0);
-    [_photo1 addSubview:self.imgviewDel1];
-    
-    deleteImage = [UIImage imageNamed:@"DeleteUploadedImage"];
-    self.imgviewDel2 = [[UIImageView alloc] initWithImage:deleteImage];
-    [self.imgviewDel2 setFrame:CGRectMake(0, 0, 15, 15)];
-    self.imgviewDel2.center = CGPointMake(CGRectGetMaxX(_photo2.bounds), 0);
-    [_photo2 addSubview:self.imgviewDel2];
-    
-    deleteImage = [UIImage imageNamed:@"DeleteUploadedImage"];
-    self.imgviewDel3 = [[UIImageView alloc] initWithImage:deleteImage];
-    [self.imgviewDel3 setFrame:CGRectMake(0, 0, 15, 15)];
-    self.imgviewDel3.center = CGPointMake(CGRectGetMaxX(_photo3.bounds), 0);
-    [_photo3 addSubview:self.imgviewDel3];
-    
-    [self refreshButton];
-}
-
-- (IBAction)starButtonClicked:(id)sender {
-    NSArray *btns = @[_star1, _star2, _star3, _star4, _star5];
-    star = [NSString stringWithFormat:@"%d", [sender tag]];
-    for (int i = 0; i < [btns count]; i++) {
-        UIButton *btn = btns[i];
-        if (i < [sender tag]) {
-             btn.selected = YES;
-        } else {
-            btn.selected = NO;
-        }
-    }
-}
+//// 为图片添加删除图标
+//- (void)addDeleteIcons
+//{
+//    UIImage *deleteImage = [UIImage imageNamed:@"DeleteUploadedImage"];
+//    self.imgviewDel1 = [[UIImageView alloc] initWithImage:deleteImage];
+//    [self.imgviewDel1 setFrame:CGRectMake(0, 0, 15, 15)];
+//    self.imgviewDel1.center = CGPointMake(CGRectGetMaxX(_photo1.bounds), 0);
+//    [_photo1 addSubview:self.imgviewDel1];
+//    
+//    deleteImage = [UIImage imageNamed:@"DeleteUploadedImage"];
+//    self.imgviewDel2 = [[UIImageView alloc] initWithImage:deleteImage];
+//    [self.imgviewDel2 setFrame:CGRectMake(0, 0, 15, 15)];
+//    self.imgviewDel2.center = CGPointMake(CGRectGetMaxX(_photo2.bounds), 0);
+//    [_photo2 addSubview:self.imgviewDel2];
+//    
+//    deleteImage = [UIImage imageNamed:@"DeleteUploadedImage"];
+//    self.imgviewDel3 = [[UIImageView alloc] initWithImage:deleteImage];
+//    [self.imgviewDel3 setFrame:CGRectMake(0, 0, 15, 15)];
+//    self.imgviewDel3.center = CGPointMake(CGRectGetMaxX(_photo3.bounds), 0);
+//    [_photo3 addSubview:self.imgviewDel3];
+//    
+//    [self refreshButton];
+//}
+//
+//- (IBAction)starButtonClicked:(id)sender {
+//    NSArray *btns = @[_star1, _star2, _star3, _star4, _star5];
+//    star = [NSString stringWithFormat:@"%d", [sender tag]];
+//    for (int i = 0; i < [btns count]; i++) {
+//        UIButton *btn = btns[i];
+//        if (i < [sender tag]) {
+//             btn.selected = YES;
+//        } else {
+//            btn.selected = NO;
+//        }
+//    }
+//}
 
 - (void)selectPhoto:(id)sender {
     if ([sender tag] - 1000 <=  [imagePaths count]) {
         [imagePaths removeObjectAtIndex:[sender tag] - 1001];
-        [self refreshButton];
+//        [self refreshButton];
     } else {
         UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:NSLocalizedString(@"取消", nil) destructiveButtonTitle:nil otherButtonTitles:NSLocalizedString(@"拍照", nil), NSLocalizedString(@"从相册选取", nil), nil];
         [actionSheet showInView:self.view];
@@ -110,53 +111,53 @@
             imgDict[@"img"] = img;
             imgDict[@"url"] = imagePath;
             [imagePaths addObject:imgDict];
-            [self refreshButton];
+//            [self refreshButton];
         }
     }];
 }
 
-- (void)refreshButton {
-    self.imgviewDel1.hidden = NO;
-    self.imgviewDel2.hidden = NO;
-    self.imgviewDel3.hidden = NO;
-    
-    if ([imagePaths count] == 0) {
-        [_photo1 setBackgroundImage:[UIImage imageNamed:@"afterSaleAdd"] forState:UIControlStateNormal];
-        self.imgviewDel1.hidden = YES;
-        _photo2.hidden = YES;
-        _photo3.hidden = YES;
-    }
-    if ([imagePaths count] == 1) {
-        [_photo2 setBackgroundImage:[UIImage imageNamed:@"afterSaleAdd"] forState:UIControlStateNormal];
-        self.imgviewDel2.hidden = YES;
-        _photo2.hidden = NO;
-        _photo3.hidden = YES;
-    }
-    if ([imagePaths count] == 2) {
-        [_photo3 setBackgroundImage:[UIImage imageNamed:@"afterSaleAdd"] forState:UIControlStateNormal];
-        self.imgviewDel3.hidden = YES;
-        _photo2.hidden = NO;
-        _photo3.hidden = NO;
-    }
-    if ([imagePaths count] == 3) {
-        _photo2.hidden = NO;
-        _photo3.hidden = NO;
-    }
-    if ([imagePaths count] > 0) {
-        for (int i = 0; i < [imagePaths count]; i++) {
-            NSDictionary *info = imagePaths[i];
-            if (i == 0) {
-                [_photo1 setBackgroundImage:info[@"img"] forState:UIControlStateNormal];
-            }
-            if (i == 1) {
-                [_photo2 setBackgroundImage:info[@"img"] forState:UIControlStateNormal];
-            }
-            if (i == 2) {
-                [_photo3 setBackgroundImage:info[@"img"] forState:UIControlStateNormal];
-            }
-        }
-    }
-}
+//- (void)refreshButton {
+//    self.imgviewDel1.hidden = NO;
+//    self.imgviewDel2.hidden = NO;
+//    self.imgviewDel3.hidden = NO;
+//    
+//    if ([imagePaths count] == 0) {
+//        [_photo1 setBackgroundImage:[UIImage imageNamed:@"afterSaleAdd"] forState:UIControlStateNormal];
+//        self.imgviewDel1.hidden = YES;
+//        _photo2.hidden = YES;
+//        _photo3.hidden = YES;
+//    }
+//    if ([imagePaths count] == 1) {
+//        [_photo2 setBackgroundImage:[UIImage imageNamed:@"afterSaleAdd"] forState:UIControlStateNormal];
+//        self.imgviewDel2.hidden = YES;
+//        _photo2.hidden = NO;
+//        _photo3.hidden = YES;
+//    }
+//    if ([imagePaths count] == 2) {
+//        [_photo3 setBackgroundImage:[UIImage imageNamed:@"afterSaleAdd"] forState:UIControlStateNormal];
+//        self.imgviewDel3.hidden = YES;
+//        _photo2.hidden = NO;
+//        _photo3.hidden = NO;
+//    }
+//    if ([imagePaths count] == 3) {
+//        _photo2.hidden = NO;
+//        _photo3.hidden = NO;
+//    }
+//    if ([imagePaths count] > 0) {
+//        for (int i = 0; i < [imagePaths count]; i++) {
+//            NSDictionary *info = imagePaths[i];
+//            if (i == 0) {
+//                [_photo1 setBackgroundImage:info[@"img"] forState:UIControlStateNormal];
+//            }
+//            if (i == 1) {
+//                [_photo2 setBackgroundImage:info[@"img"] forState:UIControlStateNormal];
+//            }
+//            if (i == 2) {
+//                [_photo3 setBackgroundImage:info[@"img"] forState:UIControlStateNormal];
+//            }
+//        }
+//    }
+//}
 
 -(void)takePhoto
 {
@@ -205,44 +206,44 @@
 }
 
 - (IBAction)sendComment:(id)sender {
-    if ([goodsArray count] == 0) {
-        return;
-    }
-    if (_commentTextView.text.length == 0) {
-        [self displayHUDTitle:nil message:@"请输入内容!"];
-        return;
-    }
-    [self displayHUD:@"加载中..."];
-    NSMutableArray *infoArray = [NSMutableArray array];
-    for (int i = 0; i < [goodsArray count]; i++) {
-        MLGoods *goods = goodsArray[i];
-        NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
-        dict[@"goodsid"] = goods.ID;
-        dict[@"unique"] = goods.unique;
-        dict[@"content"] = _commentTextView.text;
-        NSMutableArray *imgs = [NSMutableArray array];
-        for (int j = 0; j < [imagePaths count]; j++) {
-            NSDictionary *dict = imagePaths[i];
-            [imgs addObject:dict[@"url"]];
-        }
-        if ([imgs count] > 0) {
-            dict[@"images"] = imgs;
-        }
-        dict[@"stars"] = star;
-        [infoArray addObject:dict];
-    }
-    NSLog(@"%@", infoArray);
-    NSData *jsonData = [self toJSONData:infoArray];
-    NSString *jsonStr = [[NSString alloc] initWithData:jsonData
-                                              encoding:NSUTF8StringEncoding];
-    [[MLAPIClient shared] sendComment:_order.ID commentInfo:jsonStr WithBlock:^(NSDictionary *attributes, MLResponse *response) {
-        if (response.success) {
-            [self displayHUDTitle:nil message:@"评价成功"];
-            [self performSelector:@selector(back) withObject:nil afterDelay:1.0];
-            return;
-        }
-        [self displayResponseMessage:response];
-    }];
+//    if ([goodsArray count] == 0) {
+//        return;
+//    }
+//    if (_commentTextView.text.length == 0) {
+//        [self displayHUDTitle:nil message:@"请输入内容!"];
+//        return;
+//    }
+//    [self displayHUD:@"加载中..."];
+//    NSMutableArray *infoArray = [NSMutableArray array];
+//    for (int i = 0; i < [goodsArray count]; i++) {
+//        MLGoods *goods = goodsArray[i];
+//        NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+//        dict[@"goodsid"] = goods.ID;
+//        dict[@"unique"] = goods.unique;
+//        dict[@"content"] = _commentTextView.text;
+//        NSMutableArray *imgs = [NSMutableArray array];
+//        for (int j = 0; j < [imagePaths count]; j++) {
+//            NSDictionary *dict = imagePaths[i];
+//            [imgs addObject:dict[@"url"]];
+//        }
+//        if ([imgs count] > 0) {
+//            dict[@"images"] = imgs;
+//        }
+//        dict[@"stars"] = star;
+//        [infoArray addObject:dict];
+//    }
+//    NSLog(@"%@", infoArray);
+//    NSData *jsonData = [self toJSONData:infoArray];
+//    NSString *jsonStr = [[NSString alloc] initWithData:jsonData
+//                                              encoding:NSUTF8StringEncoding];
+//    [[MLAPIClient shared] sendComment:_order.ID commentInfo:jsonStr WithBlock:^(NSDictionary *attributes, MLResponse *response) {
+//        if (response.success) {
+//            [self displayHUDTitle:nil message:@"评价成功"];
+//            [self performSelector:@selector(back) withObject:nil afterDelay:1.0];
+//            return;
+//        }
+//        [self displayResponseMessage:response];
+//    }];
 }
 
 - (void)back {
@@ -278,7 +279,7 @@
 
 #pragma mark - UITableViewDelegate
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return [MLGoodsOrderTableViewCell height];
+    return 360;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -286,17 +287,25 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [_order.multiGoods count];
+    return [goodsArray count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    MLGoodsOrderTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[MLGoodsOrderTableViewCell identifier]];
-    if (!cell) {
-        cell = [[MLGoodsOrderTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:[MLGoodsOrderTableViewCell identifier]];
-        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    static NSString *CellIdentifier = @"UITableViewCell";
+    MLJudgeGoodsCell *cell = (MLJudgeGoodsCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        NSArray *nibs = [[NSBundle mainBundle]loadNibNamed:@"MLJudgeGoodsCell" owner:nil options:nil];
+        cell = [nibs lastObject];
+        [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        cell.backgroundColor = [UIColor whiteColor];
     }
-    MLGoods *goods = _order.multiGoods[indexPath.row];
-    cell.goods = goods;
+    MLGoods *goods = goodsArray[indexPath.row];
+    [cell.iconImageView setImageWithURL:[NSURL URLWithString:goods.imagePath]];
+    cell.goodsNameLabel.text = goods.name;
+    [cell.goodsNameLabel sizeToFit];
+    cell.typeLabel.text = goods.goodsPropertiesString;
+    cell.priceLabel.text = [NSString stringWithFormat:@"价格:%@", goods.price];
+    cell.numLabel.text = [NSString stringWithFormat:@"数量:%@", goods.quantityInCart];
     return cell;
 }
 
