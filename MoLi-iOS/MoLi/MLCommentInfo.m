481a527dfd79b0633903c721cbf667b24202d054
yc <ycabc1989@163.com>
2015-04-22 22:28:46 +0800

    订单评价功能完善

---
+++ MoLi-iOS/MoLi/MLCommentInfo.m
@@ -0,0 +1,13 @@
+//
+//  MLCommentInfo.m
+//  MoLi
+//
+//  Created by 颜超 on 15/4/22.
+//  Copyright (c) 2015年 zoombin. All rights reserved.
+//
+
+#import "MLCommentInfo.h"
+
+@implementation MLCommentInfo
+
+@end
--- MoLi-iOS/MoLi/MLJudgeViewController.h
+++ MoLi-iOS/MoLi/MLJudgeViewController.h
@@ -9,24 +9,12 @@
 #import <UIKit/UIKit.h>
 #import "MLOrder.h"
 #import "UIActionSheet+ZBUtilities.h"
+#import "MLJudgeGoodsCell.h"
 
 /// 评价页面.
-@interface MLJudgeViewController : UIViewController<UITextViewDelegate, UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface MLJudgeViewController : UIViewController<UITextViewDelegate, UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MLJudgeGoodsCellDelegate>
 
 @property (nonatomic, weak) MLOrder *order;
-//@property (nonatomic, weak) IBOutlet UIView *footView;+
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
-//@property (nonatomic, weak) IBOutlet UITextView *commentTextView;
-@property (nonatomic, weak) IBOutlet UILabel *placeholderLabel;
-//@property (nonatomic, weak) IBOutlet UIButton *photo1;
-//@property (nonatomic, weak) IBOutlet UIButton *photo2;
-//@property (nonatomic, weak) IBOutlet UIButton *photo3;
-//@property (nonatomic, weak) IBOutlet UIButton *star1;
-//@property (nonatomic, weak) IBOutlet UIButton *star2;
-//@property (nonatomic, weak) IBOutlet UIButton *star3;
-//@property (nonatomic, weak) IBOutlet UIButton *star4;
-//@property (nonatomic, weak) IBOutlet UIButton *star5;
-- (IBAction)starButtonClicked:(id)sender;
 - (IBAction)sendComment:(id)sender;
-- (IBAction)selectPhoto:(id)sender;
 @end
