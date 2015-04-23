a6355687349f1427fdfbf3bac7d3bf9bda3404d2
yc <ycabc1989@163.com>
2015-04-23 21:43:23 +0800

    支付高亮替换

--- MoLi-iOS/MLPayFailView.m
+++ MoLi-iOS/MLPayFailView.m
@@ -46,19 +46,19 @@
         
 		_goingPayBtn = [[UIButton alloc] initWithFrame:CGRectMake(CGRectGetWidth(frame)/2-80, CGRectGetMaxY(imageLine.frame) + 20, 80, 35)];
         [_goingPayBtn setTitle:@"重新支付" forState:UIControlStateNormal];
-        [_goingPayBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [_goingPayBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_goingPayBtn.titleLabel setFont:[UIFont systemFontOfSize:14.0]];
-        _goingPayBtn.layer.borderWidth = 1;
-        _goingPayBtn.layer.borderColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1].CGColor;
+        [_goingPayBtn setBackgroundColor:[UIColor colorWithRed:227/255.0 green:55/255.0 blue:28/255.0 alpha:1]];
         _goingPayBtn.layer.cornerRadius = 5;
         [_goingPayBtn addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_goingPayBtn];
         
         UIButton *myOrderBtn = [[UIButton alloc] initWithFrame:CGRectMake(CGRectGetMaxX(_goingPayBtn.frame)+10, CGRectGetMaxY(imageLine.frame)+20, 80, 35)];
         [myOrderBtn setTitle:@"随便逛逛" forState:UIControlStateNormal];
-        [myOrderBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [myOrderBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
         [myOrderBtn.titleLabel setFont:[UIFont systemFontOfSize:14.0]];
-        [myOrderBtn setBackgroundColor:[UIColor colorWithRed:227/255.0 green:55/255.0 blue:28/255.0 alpha:1]];
+        myOrderBtn.layer.borderWidth = 1;
+        myOrderBtn.layer.borderColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1].CGColor;
         myOrderBtn.layer.cornerRadius = 5;
         [myOrderBtn addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:myOrderBtn];
